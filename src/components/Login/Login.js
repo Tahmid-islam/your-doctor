5b357a91d762b2781995442a4f25906b67ba19db
Tahmid Islam <tahmid231@gmail.com>
2021-10-20 12:11:17 +0600

    login and registration improved

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { Link, useLocation, useHistory } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
+import loginImg from "../../images/login.png";
 
 const Login = () => {
   const { signInUsingGoogle, signInUsingEmailAndPassword, error, user } =
@@ -30,44 +31,51 @@ const Login = () => {
   }
 
   return (
-    <div className="mx-auto w-50">
-      <div className="text-center py-4">
-        <h1>
-          Login <i class="fas fa-sign-in-alt"></i>
-        </h1>
+    <div className="row container mx-auto">
+      <div className="col-md-6 ">
+        <img className="img-fluid pt-4" src={loginImg} alt="" />
       </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          className="form-control my-3 p-2"
-          placeholder="Email"
-          {...register("email", { required: true })}
-        />
-        {errors.email && <span>This field is required</span>}
-        <input
-          type="password"
-          className="form-control my-3 p-2"
-          placeholder="Password"
-          autoComplete="password"
-          {...register("password", { required: true })}
-        />
-        <p className="text-danger text-center fw-bold">{error}</p>
-        {errors.password && <span>This field is required</span>}
-        <input
-          className="form-control my-3 btn btn-primary p-2"
-          type="submit"
-          value="Login"
-        />
-      </form>
-      <Link style={{ textDecoration: "none" }} to="/register">
-        <p className="text-primary text-center fw-bold">New to Your Doctor?</p>
-      </Link>
-      <div className="text-center">
-        <button
-          onClick={handleGoogleSignIn}
-          className="btn btn-primary text-center"
-        >
-          <i className="fab fa-google"></i> Google Sign In
-        </button>
+      <div className="col-md-6">
+        <div className="text-center py-4">
+          <h1>
+            Login <i class="fas fa-sign-in-alt"></i>
+          </h1>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            className="form-control my-3 p-2"
+            placeholder="Email"
+            {...register("email", { required: true })}
+          />
+          {errors.email && <span>This field is required</span>}
+          <input
+            type="password"
+            className="form-control my-3 p-2"
+            placeholder="Password"
+            autoComplete="password"
+            {...register("password", { required: true })}
+          />
+          <p className="text-danger text-center fw-bold">{error}</p>
+          {errors.password && <span>This field is required</span>}
+          <input
+            className="form-control my-3 btn btn-primary p-2"
+            type="submit"
+            value="Login"
+          />
+        </form>
+        <Link style={{ textDecoration: "none" }} to="/register">
+          <p className="text-primary text-center fw-bold">
+            New to Your Doctor?
+          </p>
+        </Link>
+        <div className="text-center">
+          <button
+            onClick={handleGoogleSignIn}
+            className="btn btn-primary text-center"
+          >
+            <i className="fab fa-google"></i> Google Sign In
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import { useHistory, useLocation } from "react-router";
 import { Link } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
+import loginImg from "../../images/login.png";
 
 const Register = () => {
   const { signInUsingGoogle, createNewAccount, error, user } = useAuth();
@@ -29,52 +30,57 @@ const Register = () => {
   }
 
   return (
-    <div className="mx-auto w-50">
-      <div className="text-center py-4">
-        <h1>
-          Register <i class="fas fa-sign-in-alt"></i>
-        </h1>
+    <div className="row container mx-auto">
+      <div className="col-md-6">
+        <img className="img-fluid pt-4" src={loginImg} alt="" />
       </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          className="form-control my-3 p-2"
-          placeholder="Name"
-          {...register("name", { required: true })}
-        />
-        {errors.name && <span>This field is required</span>}
-        <input
-          className="form-control my-3 p-2"
-          placeholder="Email"
-          {...register("email", { required: true })}
-        />
-        {errors.email && <span>This field is required</span>}
-        <input
-          type="password"
-          className="form-control my-3 p-2"
-          placeholder="Password"
-          autoComplete="password"
-          {...register("password", { required: true })}
-        />
-        <p className="text-danger text-center fw-bold">{error}</p>
-        {errors.password && <span>This field is required</span>}
-        <input
-          className="form-control my-3 btn btn-primary p-2"
-          type="submit"
-          value="Sign in"
-        />
-      </form>
-      <Link style={{ textDecoration: "none" }} to="/login">
-        <p className="text-primary text-center fw-bold">
-          Already have an account?
-        </p>
-      </Link>
-      <div className="text-center">
-        <button
-          onClick={handleGoogleSignIn}
-          className="btn btn-primary text-center"
-        >
-          <i className="fab fa-google"></i> Google Sign Up
-        </button>
+      <div className="col-md-6">
+        <div className="text-center py-4">
+          <h1>
+            Register <i class="fas fa-sign-in-alt"></i>
+          </h1>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            className="form-control my-3 p-2"
+            placeholder="Name"
+            {...register("name", { required: true })}
+          />
+          {errors.name && <span>This field is required</span>}
+          <input
+            className="form-control my-3 p-2"
+            placeholder="Email"
+            {...register("email", { required: true })}
+          />
+          {errors.email && <span>This field is required</span>}
+          <input
+            type="password"
+            className="form-control my-3 p-2"
+            placeholder="Password"
+            autoComplete="password"
+            {...register("password", { required: true })}
+          />
+          <p className="text-danger text-center fw-bold">{error}</p>
+          {errors.password && <span>This field is required</span>}
+          <input
+            className="form-control my-3 btn btn-primary p-2"
+            type="submit"
+            value="Sign in"
+          />
+        </form>
+        <Link style={{ textDecoration: "none" }} to="/login">
+          <p className="text-primary text-center fw-bold">
+            Already have an account?
+          </p>
+        </Link>
+        <div className="text-center">
+          <button
+            onClick={handleGoogleSignIn}
+            className="btn btn-primary text-center"
+          >
+            <i className="fab fa-google"></i> Google Sign Up
+          </button>
+        </div>
       </div>
     </div>
   );
